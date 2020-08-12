a04bf5e0cb0c44e09f08bf570d672d024d0e1677
Samy Nassar <samy0ayman@gmail.com>
2020-08-12 10:37:29 +0200

    Smooth Scroll Implementation

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -96,7 +96,15 @@ section.forEach((section) => {
 */
 
 
-
+// Scroll to section on link click
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
 
 
 console.log(performance.now() - t0);
